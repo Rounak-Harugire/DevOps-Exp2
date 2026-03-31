bace3bb4f111fef382706405b8379dbe8d6a7592
Rounak Harugire <rounakharugire3435@gmail.com>
2026-03-31 12:10:43 +0530

    Add HelloDevOps.java for Jenkins CI/CD Experiment 4

---
+++ HelloDevOps.java
@@ -0,0 +1,6 @@
+public class HelloDevOps { 
+    public static void main(String[] args) { 
+        System.out.println("CI/CD Pipeline: Build Successful!"); 
+        System.out.println("Hello World from Jenkins Automation simulation."); 
+    } 
+}
